38c9215f376a3ba6d4107c77e5c5e078f33bf50d
Mathias <duble.mathias@gmail.com>
2022-09-21 09:53:02 +0200

    first commit

---
+++ back/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors())
+
+app.use('/', (_req, res) => {
+    res.json({message: 'Server Online'});
+})
+
+app.listen(8000);
